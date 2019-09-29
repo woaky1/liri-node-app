8c80d2ebea71f7c3eab943cc7be6dd16e7e4a42d
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-28 23:21:48 -0500

    got the axios call to bands in town working. Now I need to figure out what to do with the data I'm getting back.

--- liri.js
+++ liri.js
@@ -1,3 +1,41 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+
+var command = process.argv[2];
+var input = process.argv[3];
+
+switch (command) {
+    case "concert-this":
+        concertCheck(input);
+        break;
+
+    default:
+        break;
+}
+
+function concertCheck(artist){
+axios
+    .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    .then(function(response) {
+        console.log(response.data);
+    })
+    .catch(function(error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+    })
+}
